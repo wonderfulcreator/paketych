116af7646a1d3cd9a145f02c910a166aedc0b4f4
Ким Дмитрий Сергеевич <d.kim@Vostokhimvolokno.local>
2026-06-22 16:13:23 +0300

    v19: баги, политика конфиденциальности, форма согласия ПДн и картинки

--- src/app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { BoxLoader } from "@/components/BoxLoader";
-
-export default function Loading() {
-  return (
-    <div className="container flex min-h-[50vh] items-center justify-center py-20">
-      <BoxLoader label="Раскладываем пакеты по полочкам…" />
-    </div>
-  );
-}
